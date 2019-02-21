5914565afeaab98225a6ffb62bbbcaad32018b29
elmehdi007 <47179582+elmehdi007@users.noreply.github.com>
2019-02-21 20:36:17 +0100

    Add files via upload

---
+++ src/components/test/test.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class HelloMessage extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello {this.props.name}
+      </div>
+    );
+  }
+}
